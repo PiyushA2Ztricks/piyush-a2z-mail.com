5e27a230ea709e4cfb0deafbd45d7182b55a6fc4
PiyushA2Ztricks <149769315+PiyushA2Ztricks@users.noreply.github.com>
2025-06-17 13:52:23 +0530

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+  if (confirm("🚀 Visit Piyush's YouTube Channel?")) {
+    window.open("https://youtube.com/@piyush_chauhan447", "_blank");
+  } else {
+    window.open("https://t.me/PiyushA2ZTricks", "_blank");
+  }
+}, 60000);
